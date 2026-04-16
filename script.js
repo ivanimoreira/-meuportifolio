979a36e0d10bee3115d917f8760896d5fd202a29
ivanimoreira <ivannymoreira124@gmail.com>
2026-04-16 16:19:59 -0300

    v1
    Refactor 'projetos' to be an array of project objects with names and images. Update the card creation logic to display project details correctly.

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-    let projetos = ´
+  let projetos = [
+{
+nomeprojetos : "Projeto Web",
+ImgProjeto :https://github.com/ivanimoreira
+},
+{
+    nomeProjeto : "Projeto Aplicativo",
+    imgProjeto :
+}
+
+
+
+
+
+
+]
+let idProjetos = document.getElementById("projetos")
+
+projetos.map((e)=>{
+    //Aqui criamos o Card de cada Projeto
+let cardProjeto = document.createElement("div");
+
+//Aqui criamos o nome de cada projeto
+let nomeProjeto = document.createElement("h3");
+
+//Aqui criamos a imagem de cada projeto
+nomeProjeto.textContent = e.nomeProjeto;
+let imgProjeto = document.createElement("img");
+imgProjeto.src = e.imgProjeto;
+imgProjeto.style.width =  "100px"
+
+cardProjeto.appendChild(nomeProjeto);
+idProjetos.appendChild(imgProjeto);
+cardProjeto.appendChild(cardProjeto);
+
+})  let projetos = ´
